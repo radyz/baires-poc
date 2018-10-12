0929e99f94cc4eea81ff3a7b28ed6f15148abc34
Ernesto Chavez Sanchez <ejchavez88@gmail.com>
2018-10-12 03:03:47 +0000

    Configure jwt package to allow for token auth together with drf

--- poc/poc/settings.py
+++ poc/poc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
+}
--- poc/poc/urls.py
+++ poc/poc/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('^token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
